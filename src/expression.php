25cbeff962689d874510ffd2c56549f779b4f657
Dana Burkart <discodancemonkey@gmail.com>
2012-02-04 10:15:36 -0500

    Added expressions.

---
+++ src/expression.php
@@ -0,0 +1,428 @@
+<?php
+/*
+ *      expression.php
+ *      
+ *      Copyright 2010 Dana Burkart <danaburkart@gmail.com>
+ *      
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+
+/**
+ * An expression is a statement that can be evaluated to some answer. It can be
+ * an equation comparing two statements, some kind of mathematical statement, or
+ * whatever. Currently, these are operators accounted for:
+ *	+ 	: of the form a + b
+ *  - 	: of the form a - b
+ *  *	: of the form a * b
+ *  = 	: of the form a = b
+ * 	!= 	: of the form a != b
+ *  >	: of the form a > b
+ *  <	: of the form a < b
+ *  >=	: of the form a >= b
+ *  <=	: of the form a <= b
+ *	! 	: of the form !a
+ *  ()	: can encapsulate any sub-expression to change order of operations.
+ *
+ * Valid values for a and b are:
+ *	- any statement
+ *	- any numeral
+ *	- any string (eclosed by single-quotes)
+ *	- any variable satisfying this regex: /^[a-zA-Z_][a-zA-Z0-9_-]*$/
+ *
+ * @author Dana Burkart
+ */
+
+// Some defines that will be useful
+define("EXPR_VAR", 1);						// Variable
+define("EXPR_POS", 2);						// Addition
+define("EXPR_NEG", 3);						// Subtraction
+define("EXPR_MUL", 4);						// Multiplication
+define("EXPR_DIV", 5);						// Division
+define("EXPR_EQU", 6);						// Equality test
+define("EXPR_NEQ", 7);						// Non-equality test
+define("EXPR_LES", 8);						// Less-than test
+define("EXPR_GRE", 9);						// Greater-than test
+define("EXPR_LEQ", 10);						// Less-than-or-equal test
+define("EXPR_GEQ", 11);						// Greater-than-or-equal test
+define("EXPR_NOT", 12);						// Not operator
+
+// An atom is any entity that can't be broken down further. Operators, variables,
+// numbers, and strings are all atoms.
+class atom {
+	public $type;
+	public $val;
+	public $width;
+	
+	public function __construct($type, $val = '') {
+		$this->type = $type;
+		$this->val = $val;
+		
+		$this->width = 1;
+		if ($this->type == EXPR_NEQ || $this->type == EXPR_LEQ || 
+			$this->type == EXPR_GEQ) {
+			$this->width = 2;	
+		}
+	}
+}
+
+class expression {
+	private $expression 	= '';
+	private $atomList 		= array();
+	private $eval			= null;
+	
+	public function __construct($expr) {
+		$this->expression = $expr;
+		$this->atomList = $this->decompose($expr);
+	}
+	
+	/**
+	 * Evaluates the decomposed expression contained in $this->atomList.
+	 *
+	 * @param registry an associative array containing relevant variables
+	 * @return the result of the evaluation.
+	 */
+	public function evaluate(&$registry=null, $recurse=false) {
+		// Back ourself up; this expression may need to be called more than
+		// once.
+		if ( !$recurse ) {
+			$this->eval = null;
+			$this->atomListBackup = $this->atomList;
+		}
+		
+		// Get the next atom
+		$next = array_pop($this->atomList);
+		
+		if (is_null($this->eval) || $recurse) {
+			switch($next->type) {
+				case EXPR_VAR:
+					if (is_numeric($next->val)) {
+						$this->eval = $next->val;
+						
+						if ($recurse)
+							return $this->eval;
+					} else if (preg_match('/\'([a-zA-Z0-9_-]*)\'/', $next->val, $matches)) {
+						$this->eval = $matches[1]; 
+						
+						if ($recurse)
+							return $this->eval;
+					} else if ($next->val == 'true' || $next->val == 'false') {
+						$this->eval = (bool)$next->val;
+						
+						if ($recurse)
+							return $this->eval;
+					} else if (isset($registry[$next->val])) {
+						$this->eval = $registry[$next->val];
+						
+						if ($recurse)
+							return $this->eval;
+					} else {
+						$this->eval = 0;
+						
+						if ($recurse)
+							return $this->eval;
+					}
+						
+					break;
+				case EXPR_NOT:
+					$this->eval = !($this->evaluate($registry, true));
+					
+					if ($recurse)
+						return $this->eval;
+					break;
+				case EXPR_MUL:
+					$b = $this->evaluate($registry, true);
+					$this->eval = $this->evaluate($registry, true) * $b;
+					
+					if ($recurse)
+						return $this->eval;
+					break;
+				case EXPR_DIV:
+					$b = $this->evaluate($registry, true);
+					$this->eval = $this->evaluate($registry, true) / $b;
+					
+					if ($recurse)
+						return $this->eval;
+					break;
+				case EXPR_POS:
+					$this->eval = $this->evaluate($registry, true) + $this->evaluate($registry, true);
+					
+					if ($recurse)
+						return $this->eval;
+					break;
+				case EXPR_NEG:
+					$b = $this->evaluate($registry, true);
+					$this->eval = $this->evaluate($registry, true) - $b;
+					
+					if ($recurse)
+						return $this->eval;
+					break;
+				case EXPR_EQU:
+					$this->eval = ($this->evaluate($registry, true) == $this->evaluate($registry, true));
+					break;
+				case EXPR_NEQ:
+					$this->eval = ($this->evaluate($registry, true) != $this->evaluate($registry, true));
+					break;
+				case EXPR_LES:
+					$this->eval = ($this->evaluate($registry, true) > $this->evaluate($registry, true));
+					break;
+				case EXPR_GRE:
+					$this->eval = ($this->evaluate($registry, true) < $this->evaluate($registry, true));
+					break;
+				case EXPR_LEQ:
+					$this->eval = ($this->evaluate($registry, true) >= $this->evaluate($registry, true));
+					break;
+				case EXPR_GEQ:
+					$this->eval = ($this->evaluate($registry, true) <= $this->evaluate($registry, true));
+					break;
+			}
+			
+			// Restore our stack
+			$this->atomList = $this->atomListBackup;
+			return $this->eval;
+		} else {
+			return $this->eval;
+		}
+	}
+	
+	/**
+	 * decompose() checks for an equal sign, and if found, decomposes each side
+	 * using decomposeE(), and then pushes the equality operator onto the stack.
+	 * If there is no equal sign, then decomposeE() is called on the entire
+	 * expression.
+	 *
+	 * @param expr the expression to decompose
+	 */
+	public function decompose($expr) {
+		$pos = null;
+		
+		$i = 0;
+		$count = strlen($expr);
+		$atom;
+		
+		while ($i < $count) {
+			if ($expr[$i] == '=') {
+				if (!is_null($pos))
+					die('Malformed equation: Only one terminal allowed.');
+				$pos = $i;
+				
+				if ($expr[$pos - 1] == '!') 
+					$atom = new atom(EXPR_NEQ);
+				else
+					$atom = new atom(EXPR_EQU);
+			} else if ($expr[$i] == '>') {
+				if (!is_null($pos))
+					die('Malformed equation: Only one terminal allowed.');
+				$pos = $i;
+				
+				if ($expr[$pos + 1] == '=') {
+					$atom = new atom(EXPR_GEQ);
+					$pos += 1;
+					$i += 1;
+				} else {
+					$atom = new atom(EXPR_GRE);
+				}
+			} else if ($expr[$i] == '<') {
+				if (!is_null($pos))
+					die('Malformed equation: Only one terminal allowed.');
+				$pos = $i;
+				
+				if ($expr[$pos + 1] == '=') {
+					$atom = new atom(EXPR_LEQ);
+					$pos += 1;
+					$i += 1;
+				} else {
+					$atom = new atom(EXPR_LES);
+				}
+			}
+			$i++;
+		}
+		
+		if (!is_null($pos) && $atom->width == 2) {
+			$expr1 = substr($expr, 0, $pos - 1);
+			$expr2 = substr($expr, $pos + 1, $count - ($pos + 1));
+			
+			list($whocares, $this->atomList) = $this->decomposeE($expr1, 0);
+			list($whocares, $lst) = $this->decomposeE($expr2, 0);
+			$this->atomList = array_merge($this->atomList, $lst);
+			$this->atomList[] = $atom;
+			
+			return $this->atomList;
+		} else if (!is_null($pos)) {
+			$expr1 = substr($expr, 0, $pos);
+			$expr2 = substr($expr, $pos + 1, $count - ($pos + 1));
+			
+			list($whocares, $this->atomList) = $this->decomposeE($expr1, 0);
+			list($whocares, $lst) = $this->decomposeE($expr2, 0);
+			$this->atomList = array_merge($this->atomList, $lst);
+			$this->atomList[] = $atom;
+		
+			return $this->atomList;
+		} else {
+			list($whocares, $this->atomList) = $this->decomposeE($expr, 0);
+			
+			return $this->atomList;
+		}
+	}
+	
+	/**
+	 * decomposeE() decomposes the given expression.
+	 *
+	 * @param expr the expression to decompose
+	 * @param i the offset we are starting from
+	 */
+	public function decomposeE($expr, $i) {
+		$buffer = '';
+		$atomList = array();
+		
+		$count = strLen($expr);
+		while ($i < $count) {
+			switch ($expr[$i]) {
+				case '(':
+					$buffer = trim($buffer);
+					if (!empty($buffer))
+						die("Incorrect Syntax: \"$buffer\" not allowed directly before \"(\"");
+					list($i, $lst) = $this->decomposeParenE($expr, $i);
+					$atomList = array_merge($atomList, $lst);
+					break;
+				case '!':
+					$buffer = trim($buffer);
+					if (!empty($buffer)) {
+						$atomList[] = $this->newVar($buffer);
+						$buffer = '';
+					}
+					list($i, $lst) = $this->decomposeE($expr, $i+1);
+					$atomList = array_merge($atomList, $lst);
+					$atomList[] = new atom(EXPR_NOT);
+					break;
+				case '*':
+					if (!empty($buffer)) {
+						$atomList[] = $this->newVar($buffer);
+						$buffer = '';
+					}
+					list($i, $lst) = $this->decomposeE($expr, $i+1);
+					$atomList = array_merge($atomList, $lst);
+					$atomList[] = new atom(EXPR_MUL);
+					break;
+				case '/':
+					if (!empty($buffer)) {
+						$atomList[] = $this->newVar($buffer);
+						$buffer = '';
+					}
+					list($i, $lst) = $this->decomposeE($expr, $i+1);
+					$atomList = array_merge($atomList, $lst);
+					$atomList[] = new atom(EXPR_DIV);
+					break;
+				case '+':
+					if (!empty($buffer)) {
+						$atomList[] = $this->newVar($buffer);
+						$buffer = '';
+					}
+					list($i, $lst) = $this->decomposeE($expr, $i+1);
+					$atomList = array_merge($atomList, $lst);
+					$atomList[] = new atom(EXPR_POS);
+					break;	
+				case '-':
+					if (!empty($buffer)) {
+						$atomList[] = $this->newVar($buffer);
+						$buffer = '';
+					}
+					list($i, $lst) = $this->decomposeE($expr, $i+1);
+					$atomList = array_merge($atomList, $lst);
+					$atomList[] = new atom(EXPR_NEG);
+					break;	
+				default:
+					$buffer = $buffer . $expr[$i];
+					break;
+			}
+			$i++;
+		}
+		
+		if (!empty($buffer)) {
+			$atomList[] = $this->newVar($buffer);
+		}
+		
+		return array($i, $atomList);
+	}
+	
+	/**
+	 * decomposeParenE() decomposes a parenthesized expression, returning the
+	 * resultant stack.
+	 * 
+	 * @param expr the expression to decompose
+	 * @param i the offset into the expression the parenthesized expression
+	 *		occurs.
+	 *
+	 * @return a pair containing the offset to the end of the parenthesized
+	 *		expression and the stack of atoms that were decomposed
+	 */
+	public function decomposeParenE($expr, $i) {
+		$buffer = '';
+		$atomList = array();
+		
+		$parenCount = 1;
+		$i += 1;
+		$beg = $i;
+		
+		$count = strlen($expr);
+		while ($i < $count && $parenCount > 0) {
+			switch ($expr[$i]) {
+				case '(':
+					$parenCount += 1;
+					break;
+				case ')':
+					$parenCount -= 1;
+					break;
+			}
+			$i++;
+		}
+		
+		if ($parenCount > 0)
+			die("Incorrect Syntax: You are missing $parenCount closing parentheses");
+		if ($parenCount < 0)
+			die("Incorrect Syntax: You are missing ".abs($parenCount)."opening parentheses");
+		
+		list($whocares, $atomList) = $this->decomposeE(substr($expr, $beg, $i - $beg - 1), 0);
+		return array($i, $atomList);
+	}
+	
+	/**
+	 * Creates a new variable atom, subsequent to validating that the string
+	 * is correct for a variable
+	 *
+	 * @param str the name of the variable
+	 * @return the newly created atom
+	 */
+	public function newVar($str) {
+		$str = trim($str);
+		
+		// Try to make it a variable
+		if (preg_match('/^[a-zA-Z0-9\'_][a-zA-Z0-9\'_-]*$/', $str))
+			return new atom(EXPR_VAR, $str);
+		else {
+			return false;
+		}
+	}
+	
+	public function getExpressionId() {
+		return preg_replace('/\s/', '', $this->expression);
+	}
+	
+	public function getExpression() {
+		return $this->expression;
+	}
+}
+
+?>
--- src/scurvy.php
+++ src/scurvy.php
@@ -25,7 +25,7 @@
  * includese, loops, and if statements. Here is the syntax:
  * 		{variable}
  *		{include somefile.html}
- * 		{if var}
+ * 		{if expression}
  * 		...
  * 		{/if}
  * 		{foreach assocArr}
@@ -38,13 +38,17 @@
  * 		  everything
  *
  * \author Dana Burkart
- */		
+ */	
+ 
+require_once 'expression.php';
+
 class Scurvy {
 	private $RE_VAR = '/\{([a-zA-Z0-9_]+)\}/';
+	private $RE_EXPR = '/\{([a-zA-Z0-9_=\>\<\-\+\(\)\s\'\!\*]+)\}/';
 	private $RE_INC = '/\{include\s([a-zA-Z0-9_.\/]+)\}/';
 	private $RE_FOR_BEG = '/\{foreach\s([a-zA-Z0-9_]+)\}/';
 	private $RE_FOR_END = '/\{\/foreach\}/';
-	private $RE_IF_BEG = '/\{if\s([a-zA-Z0-9_]+)\}/';
+	private $RE_IF_BEG = '/\{if\s([a-zA-Z0-9_=\>\<\-\+\(\)\s\'\!\*]+)\}/';
 	private $RE_IF_END = '/\{\/if\}/';
 
 	private $name;
@@ -57,6 +61,7 @@ class Scurvy {
 	private $incTemplates;
 	private $forTemplates;
 	private $ifTemplates;
+	private $expressions;
 	
 	private $template_dir;
 	
@@ -72,38 +77,52 @@ class Scurvy {
 		} else
 			die("template: $fuzzyType is not a valid file or array");
 		
-		$this->instances = new frameworkInstances($template_dir);
-		
+		// Initialize our variables
 		$this->template_dir = $template_dir;
 		$this->vars = array();
 		$this->forTemplates = array();
 		$this->ifTemplates = array();
 		$this->incTemplates = array();
+		$this->expressions = array();
+		
+		// Parse the template
 		$this->parse();
 	}
 
+	/**
+	 * The render function goes through and renders the parsed document.
+	 *
+	 * @return string containing the rendered output.
+	 */
 	public function render() {
 		$strings = implode($this->strings);
 		
+		// Render includes
 		foreach ($this->incTemplates as $path => $include) {
 			$incOutput = $include->render();
 			$path = preg_replace("/\//", "\/", $path);
 			$strings = preg_replace("/\{include\s$path\}/", $incOutput, $strings);
 		}
-
+		
+		// Render if-statements
 		foreach ($this->ifTemplates as $key => $sub) {
 			foreach ($sub as $iKey => $instance) {
 				$str = '';
-				if (isset($this->vars[$key]) && $this->vars[$key] == true)
+				
+				if ($this->expressions[$key]->evaluate($this->vars)) {
 					$str = $instance->render();
-					
-				$strings = preg_replace("/\{if:$key:$iKey\}/", $str, $strings);
+				}
+				
+				$pregKey = preg_quote($key);
+				$strings = preg_replace("/\{if:$pregKey:$iKey\}/", $str, $strings);
 			}
 		}
 		
+		// Render foreach-loops
 		foreach ($this->forTemplates as $key => $sub) {
 			foreach ($sub as $iKey => $instance) {
 				$tmplGen = '';
+				
 				if (isset($this->vars[$key]) && is_array($this->vars[$key])) {
 					foreach ($this->vars[$key] as $assocArray) {
 						foreach ($assocArray as $k => $v)
@@ -111,13 +130,24 @@ class Scurvy {
 						$tmplGen .= $instance->render();
 					}
 				}
-				$strings = preg_replace("/\{for:$key:$iKey\}/", $tmplGen, $strings);
+				
+				$pregKey = preg_quote($key);
+				$strings = preg_replace("/\{for:$pregKey:$iKey\}/", $tmplGen, $strings);
 			}
 		}
 		
+		// Render plain old variables
 		foreach ($this->vars as $key => $val) {
 			if (!is_array($val))
 				$strings = preg_replace("/\{$key\}/", $val, $strings);
+		}
+		
+		// Render expressions
+		foreach ($this->expressions as $key => $expr) {
+			$eval = (string)$expr->evaluate($this->vars);
+			$pregKey = preg_quote($key);
+			
+			$strings = preg_replace('/\{'.$pregKey.'\}/', $eval, $strings);
 		}
 		
 		$strings = preg_replace("/\\\{/", '{', $strings);
@@ -126,13 +156,16 @@ class Scurvy {
 		return $strings;
 	}
 
+	/**
+	 * Sets the value of a variable.
+	 *
+	 * @param var variable to set
+	 * @param val value to set var to
+	 */
 	public function set($var, $val) {
-		//if (is_array($val) && !isset($this->forTemplates[$var]))
-			//-- Replace with more robust error handling
-			//die("'$var' does not accept arrays");
-		
 		$this->vars[$var] = $val;
 		
+		// We also need to set $var on each sub-template. So recursively do that
 		foreach($this->forTemplates as $templateBunch)
 			foreach($templateBunch as $sub)
 				$sub->set($var, $val);
@@ -147,10 +180,19 @@ class Scurvy {
 
 	//--- Private functions --------------------------------------------------//
 	
+	/**
+	 * Parse the template. The process here is basically to create sub-templates
+	 * for some of the language constructs, and tie them in with placeholders in
+	 * their parent template. Later, when we call render(), we can put the
+	 * output of sub-templates back into their parents.
+	 */
 	private function parse() {
 		$count = count($this->strings);
 		for ($i = 0; $i < $count; $i++) {
 			$matches;
+			
+			// Find all the variables. If the variable doesn't exist in our
+			// dictionary, stub it out.
 			preg_match_all($this->RE_VAR, $this->strings[$i], $matches);
 			if (!empty($matches[0])) {
 				foreach ($matches[1] as $match) {
@@ -159,9 +201,13 @@ class Scurvy {
 				}
 			}
 
+			// Find all foreach-loops. Every time we find one, make a new
+			// template with the contents of that loop, and put a placeholder
+			// where it used to be.
 			$n = preg_match($this->RE_FOR_BEG, $this->strings[$i], $matches);
 			if ($n > 0) {
 				$forName = $matches[1];
+				
 				if (!isset($this->forTemplates[$forName]))
 					$this->forTemplates[$forName] = array();
 				$n = count($this->forTemplates[$forName]);
@@ -171,18 +217,31 @@ class Scurvy {
 				$this->strings[$i] = '{for:'.$forName.':'.$n.'}';
 			}
 
+			// Do the same general process for our if statement.
 			$n  = preg_match($this->RE_IF_BEG, $this->strings[$i], $matches);
 			if ($n > 0) {
-				$ifName = $matches[1];
-				if (!isset($this->ifTemplates[$ifName]))
-					$this->ifTemplates[$ifName] = array();
-				$n = count($this->ifTemplates[$ifName]);
-				$this->ifTemplates[$ifName][$n] = $this->parseRecursive($i,
+				$ifExpr = new expression($matches[1]);
+				$exprId = $ifExpr->getExpressionId();
+				
+				if (!isset($this->expressions[$exprId]))
+					$this->expressions[$exprId] = $ifExpr;
+				
+				if (!isset($this->ifTemplates[$exprId]))
+					$this->ifTemplates[$exprId] = array();
+				$n = count($this->ifTemplates[$exprId]);
+				$this->ifTemplates[$exprId][$n] = $this->parseRecursive($i,
 					array($this->RE_IF_BEG, $this->RE_IF_END),
-					"{$this->name}_if_{$ifName}_$n");
-				$this->strings[$i] = '{if:'.$ifName.':'.$n.'}';
+					"{$this->name}_if_{$exprId}_$n");
+				$this->strings[$i] = '{if:'.$exprId.':'.$n.'}';
 			}
 			
+			// Currently, we have the same general process for includes. But..
+			// there are problems with this approach. In the future, this could
+			// be refactored.
+			//
+			// TODO: Move this block of code to the beginning, and instead of
+			// creating a template, just replace the include statement with
+			// the file contents of the include.
 			preg_match_all($this->RE_INC, $this->strings[$i], $matches);
 			if (!empty($matches[0])) {
 				foreach($matches[1] as $match) {
@@ -199,9 +258,34 @@ class Scurvy {
 					}
 				}
 			}
+			
+			// Find all expressions. We could probably get rid of this feature
+			// because expressions probably won't be used outside of
+			// if-statements.
+			preg_match_all($this->RE_EXPR, $this->strings[$i], $matches);
+			if (!empty($matches[0])) {
+				foreach ($matches[1] as $match) {
+					$expr = new expression($match);
+					$exprId = $expr->getExpressionId();
+					
+					if (!isset($this->expressions[$exprId]))
+						$this->expressions[$exprId] = $expr;
+						
+					$this->strings[$i] = preg_replace('/\{'.preg_quote($expr->getExpression()).'\}/', '{'.$exprId.'}', $this->strings[$i]);
+				}
+			}
 		}
 	}
 
+	/**
+	 * This function is used by the parse function to grab the contents of 
+	 * scurvy block statements.
+	 *
+	 * @param start the start index into $this->strings
+	 * @param regex the regular expression for the block statement
+	 * @param subName the name of the subTemplate (for debugging purposes)
+	 * @return a new template with the contents of the block statement
+	 */
 	private function parseRecursive($start, $regex, $subName = 'template') {
 		$re_beg = $regex[0];
 		$re_end = $regex[1];
@@ -209,6 +293,7 @@ class Scurvy {
 		$numLines = count($this->strings);
 		$i = $start + 1;
 		$n = 1;
+		$line = 0;
 		
 		for ($i; $i < $numLines; $i++) {
 			$match = preg_match($re_beg, $this->strings[$i]);
@@ -222,10 +307,11 @@ class Scurvy {
 					if ($n == 0) break;
 				}
 			}
+			$line++;
 		}
 
 		if ($n > 0) {
-			echo "Opening brace on line $line of {$this->name} has no closing brace\n<br />";
+			echo "Opening brace on line $line of {$subName} has no closing brace\n<br />";
 		}
 
 		$subTmpl = array_slice($this->strings, $start + 1, $i - ($start + 1));
@@ -242,10 +328,9 @@ class Scurvy {
 	private function require_file($file) {
 		if (file_exists($this->template_dir . $file)) {
 			require_once $this->template_dir . $file;
-		} else {
-			$location = $this->instances->findTemplate($file);
-			require_once $location . $file;
+			return true;
 		}
+		return false;
 	}
 }
 
